62bb631ecf8f2a92b85bea9275daad287420847c
Just-MohitChavan <mohitchavan127@gmail.com>
2026-03-21 15:28:16 +0530

    added data

--- book.cpp
+++ book.cpp
@@ -2,5 +2,55 @@
 using namespace std;
 
 class Book{
-    
-}
+    int id;
+    string title;
+    string author;
+    string subject;
+    string isbn;
+    float price;
+
+    public:
+        Book(void): id(0), title(""), author(""), subject(""), isbn(""), price(0.0){}
+        
+        Book(int id, string title, string author, string subject, string isbn, float price): id(id), title(title), author(author), subject(subject), isbn(isbn), price(price){}
+
+        virtual void input(){
+            cout<<"Enter id: ";
+            cin>>id;
+            cout<<"Enter title: ";
+            cin>>title;
+            cout<<"Enter author: ";
+            cin>>author;
+            cout<<"Enter subject: ";
+            cin>>subject;
+            cout<<"Enter isbn: ";
+            cin>>isbn;
+            cout<<"Enter price: ";
+            cin>>price;
+        }
+
+        int getBookId() const{
+            return id;
+        }
+
+        string getBookTitle() const{
+            return title;
+        }
+
+        string getBookAuthor() const{
+            return author;
+        }
+
+        string getBookSubject() const{
+            return subject;
+        }
+
+        string getBookIsbn() const{
+            return isbn;
+        }
+
+        float getBookPrice() const{
+            return price;
+        }
+
+};
