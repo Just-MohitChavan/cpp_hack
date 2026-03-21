6bd03ec8f435dee5e198160c890198935138bcba
D1-96454-varsha <varshareddy0206@gmail.com>
2026-03-21 15:44:13 +0530

    done issuebook

--- isssueBook.cpp
+++ isssueBook.cpp
@@ -1,44 +1,24 @@
 #include<iostream>
 #include <string>
+#include <ctime>
+#include"copy.cpp"
+
 using namespace std;
-class Date
-{
-    private: 
-    int day; 
-    int month; 
-    int year; 
-    public: 
-    Date( void ) : day(0) , month(0) , year(0)
-    {   }
-    Date(int day, int month , int year) : day(day) , month(month) , year(year)
-    {   }
-    void acceptDate(void )
-    {
-        cout<<"Day : "; 
-        cin>>day;
-        cout<<"Month : "; 
-        cin>>month; 
-        cout<<"Year : "; 
-        cin>>year;
-    }
-    void printDate( void )
-    {
-        cout<<day<<"/"<<month<<"/"<<year<<endl; 
-    }
-}; 
+
+
 class member{
     private:
     int memberId;
     string memberName;
     string memberemail;
     string memberPhone;
-    Date nextpayment_duedate;
+    time_t nextpayment_duedate;
     bool paid_satatus;
 
     public:
     member ( void ) : memberId(0) , memberName("") , memberemail("") , memberPhone("") , paid_satatus(false)
     {   }
-    member (int memberId , string memberName , string memberemail , string memberPhone , Date nextpayment_duedate , bool paid_satatus) : memberId(memberId) , memberName(memberName) , memberemail(memberemail) , memberPhone(memberPhone) , nextpayment_duedate(nextpayment_duedate) , paid_satatus(paid_satatus)
+    member (int memberId , string memberName , string memberemail , string memberPhone , time_t nextpayment_duedate , bool paid_satatus) : memberId(memberId) , memberName(memberName) , memberemail(memberemail) , memberPhone(memberPhone) , nextpayment_duedate(nextpayment_duedate) , paid_satatus(paid_satatus)
     {   }
 
     bool get_paid_status()
@@ -70,28 +50,50 @@ class member{
     }
 };
 
-class issueRecord : public member{
+class issueRecord : public member, public Copy{
     private:
     int issueId;
-    Date issueDate;
-    Date return_date;
-    Date return_duedate;
+    time_t issueDate;
+    time_t return_date;
+    time_t return_duedate;
     float fine_ammount;
 
     public:
+    issueRecord ( void ) : issueId(0) , issueDate(0) , return_date(0) , return_duedate(0) , fine_ammount(0.0)
+    {   }
+    issueRecord (int issueId , time_t issueDate , time_t return_date , time_t return_duedate , float fine_ammount) : issueId(issueId) , issueDate(issueDate) , return_date(return_date) , return_duedate(return_duedate) , fine_ammount(fine_ammount)
+    {   }
 
-    void paymentStatus()
+    void checkPaymentStatus()
     {
         if(get_paid_status())
         {
             cout<<"Payment Status : Paid"<<endl; 
+            searchAvailableCopy();
         }
         else
         {
             cout<<"Payment Status : Not Paid"<<endl; 
         }
     }
-    
+    void searchAvailableCopy () 
+    {
+        cout<<"Searching for available copy of the book..."<<endl; 
+        if(Copy::getCopyStatus()=="available") 
+        {
+            cout<<"Available copy found. You can issue the book."<<endl; 
+            
+
+            
+        }
+        else
+        {
+            cout<<"No available copy found. Please try again later."<<endl; 
+        }
+
+    }
+
+
 
     
 };
